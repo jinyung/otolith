f51ae693a9741c2079a17d780c46687ebfd41983
jinyung <wongjinyung@gmail.com>
2014-06-30 18:04:41 +0800

    addition of agglda (and its internal .kflda)

---
+++ R/agglda.R
@@ -0,0 +1,172 @@
+#' LDA model aggregation 
+#' 
+#' @description aggregate LDA models based on iterated k-fold resampling method 
+#' @param X matrix/ dataframe of predictors, e.g. EFA coefficients/ PC scores 
+#'  selected using \code{\link{selectdim}}
+#' @param Y factor/ character giving the class, e.g. value obtained from 
+#'   \code{\link{getclass}} or \code{sp} value from \code{\link{routine1}}
+#'   object
+#' @param newdata matrix/ dataframe of newdata to be predicted. see details.
+#' @param type prediction based on the majority vote (\code{"vote"}) from the 
+#'  submodels or based on the mean posterior probability from the submodels 
+#'  (\code{"post"})
+#' @param k the fold number for k-fold resampling
+#' @param run the iteration number for iteration of k-fold resampling
+#' @param threshold single numeric value of < 1. threshold of the proportion of 
+#'   majority vote or the mean posterior probability. predictions with less than
+#'   the value will be reported as \code{NA}
+#' @param prior the prior used in \code{\link[MASS]{lda}} models, \code{"equal"}
+#'   means all classes have same prior, \code{"proportion"} means prior
+#'   according to the classes weight in the training data.
+#' @param suppress logical. whether to suppress the progress monitoring output
+#' @details 
+#'   If \code{newdata} is provided, the function is in the \emph{prediction
+#'   mode}, the aggregated model will be built from \code{X} and \code{Y} and 
+#'   predicition is performed on \code{newdata}. Otherwise, if \code{newdata =
+#'   NULL} (default) the function is in \emph{evaluation mode}.
+#'   
+#'   In \emph{evaluation mode}, overall accuracy of the model and the by-class
+#'   statistics are calculated, similar to that of \code{\link{mrkfcv2}}. 
+#'   However, the statistics are calculated based on the aggregated prediction.
+#'   See reference for explanation on model aggregation and thresholding.  
+#' @return
+#'  \item{accuracy}{[\emph{evaluation mode}] the overall accuracy in percent}
+#'  \item{conmat}{[\emph{evaluation mode}] confusion matrix}
+#'  \item{stat}{[\emph{evaluation mode}] matrix containing the statistics of
+#'    each class, see details}
+#'  \item{total}{[\emph{evaluation mode}] the total percent of reported
+#'   prediction after threshold. give \code{NULL} if \code{threshold} is not
+#'   given}
+#'  \item{ind.prediction}{matrix containing the prediction result on each 
+#'    training/ new specimens}  
+#' @importFrom MASS lda
+#' @seealso \code{\link{kfcv}}, \code{\link{mrkfcv}}
+#' @references 
+#'  Beleites, C., & Salzer, R. (2008). Assessing and improving the stability of
+#'  chemometric models in small sample size situations. \emph{Analytical and
+#'  Bioanalytical Chemistry}, 390(5), 1261-1271.
+#' @export
+
+agglda <- function (X, Y, newdata = NULL, type = c("vote", "post"), 
+                    k = 5, run = 100, threshold, 
+                    prior = c("equal", "proportion"), suppress = FALSE) {
+  # initialize
+  type <- match.arg(type)
+  prior <- match.arg(prior)
+  Y <- factor(Y)
+  class.length <- length(levels(Y))
+  if (is.null(newdata)) {
+    agg.class.all <- as.data.frame(matrix(data = NA, run, dim(X)[1], 
+                                          dimnames = list(paste0("run", 1:run), 
+                                                          rownames(X))))
+    agg.posterior.all <- array(data = NA, dim = c(dim(X)[1], class.length, 
+                         run), dimnames = list(rownames(X), levels(Y), 
+                         paste0("run-", 1:run)))
+  } else {
+    krun.dimname <- paste(rep(paste0("run", 1:run), each = k),
+                          paste0("fold", 1:k), sep="-")
+    agg.class.all <- as.data.frame(matrix(data = NA, run * k, dim(newdata)[1], 
+                                          dimnames = list(krun.dimname, 
+                                                          rownames(newdata))))
+    agg.posterior.all <- array(data = NA, dim = c(dim(newdata)[1], class.length, 
+                         run * k), dimnames = list(rownames(newdata), levels(Y), 
+                                                 krun.dimname))
+  }
+  # progress bar
+  if (suppress == FALSE) {
+    cat("\n **Running ", run, "iterations ")
+    pb <- txtProgressBar(1, run, style=3, char="|")
+  }
+  # going thru iterations
+  for (i in 1:run) {
+    run.i <- .kflda(X = X, Y = Y, k = k, newdata = newdata, 
+                    prior = prior)
+    if (is.null(newdata)) {
+      agg.class.all[i, ] <- run.i$agg.class
+      agg.posterior.all[, , i] <- run.i$agg.posterior
+    } else {
+      subindex <- (((i - 1) * k) + 1):(k * i)
+      agg.class.all[subindex, ] <- run.i$agg.class
+      agg.posterior.all[, , subindex] <- run.i$agg.posterior
+    }
+    if (suppress == FALSE)
+      setTxtProgressBar(pb, i)
+  }
+  # table vote counts/ mean posterior of each candidates across runs
+  vprediction <- switch(type, 
+                        vote = vapply(agg.class.all, function (x) 
+                          summary(factor(x, levels=levels(Y))), 
+                          integer(class.length)), 
+                          # this return a matrix of summary (summary on each col)
+                        post = t(apply(agg.posterior.all, c(1, 2), 
+                                        mean, na.rm = TRUE)))
+  # calculate sd for mean posterior too
+  if (type == "post")
+    posterior.sd <- t(apply(agg.posterior.all, c(1, 2), 
+                            sd, na.rm = TRUE))
+  # announce the winner
+  prediction.class <- levels(Y)[apply(vprediction, 2, which.max)]
+  # highlight the correct candidate/ winner's result
+  if (is.null(newdata)) { # get the vote %/ mean posterior of correct candidate
+    prediction.stat <- mapply(function(x, y) x[which(levels(y) == y)], 
+                              data.frame(vprediction), Y)
+    if (type == "post")
+      posterior.sd <- mapply(function(x, y) x[which(levels(y) == y)], 
+                             data.frame(posterior.sd), Y)
+  } else { # get whatever the highest (winner) since dont know which is correct
+    prediction.stat <- apply(vprediction, 2, max)
+    if (type == "post")
+      posterior.sd <- mapply(function(x, y) x[y], data.frame(posterior.sd), 
+                               apply(vprediction, 2, which.max))
+  }
+  # then get the proportion (for posterior, value did not change)
+  prediction.percent <- prediction.stat / apply(vprediction, 2, sum)
+  # thresholding based on the proportion
+  if (!missing(threshold)) {
+    prediction.class[prediction.percent < threshold] <- NA
+    pred.length <- length(prediction.class[!is.na(prediction.class)])
+    total.pred <- pred.length / dim(X)[1] * 100
+  } else {
+    total.pred <- NULL
+    pred.length <- dim(X)[1]
+  }
+  if (is.null(newdata)) {
+    ind.prediction <- prediction.class == Y
+    accuracy <- sum(ind.prediction, na.rm = TRUE) / pred.length * 100
+    result <- data.frame(ID = dimnames(X)[[1]],
+                             species = Y, predict = prediction.class, 
+                             correct = ind.prediction)
+    # confusion matrix and stats
+    cm <- as.matrix(as.data.frame.matrix(table(prediction.class, Y)))
+    TP <- diag(cm)
+    TN <- sapply(c(1:class.length), function(x) sum(cm[ -x, -x]))
+    FP <- rowSums(cm) - TP
+    FN <- colSums(cm) - TP
+    recall <- TP / (TP + FN) # recall
+    precision <- TP / (TP + FP) # precision
+    specificity <- TN / (TN + FP) # specificity
+    prevalence <- colSums(cm) / sum(cm)
+    PosPredVal <- (recall * prevalence) / ((recall * prevalence) + 
+                    ((1 - specificity) * (1 - prevalence)))
+    NegPredVal <- (specificity * (1 - prevalence)) / 
+                    (((1 - recall) * prevalence) + 
+                    ((specificity) * (1 - prevalence)))
+    stat <- round(cbind(recall, precision, specificity, 
+                        PosPredVal, NegPredVal), 2)    
+    # different % name for different method
+    switch(type, vote = {result$vote.percent <- round(prediction.percent, 3)},
+           post = {result$mean.posterior <- round(prediction.percent, 3)
+                   result$sd.posterior <- round(posterior.sd, 3)})
+    result <- list(accuracy=accuracy, conmat = cm, stat = stat, 
+                   total = total.pred, ind.prediction = result)
+  } else {
+    result <- data.frame(predict = prediction.class)
+    switch(type, vote = {result$vote.percent <- round(prediction.percent, 3)},
+           post = {result$mean.posterior <- round(prediction.percent, 3)
+                   result$sd.posterior <- round(posterior.sd, 3)})
+    rownames(result) <- rownames(newdata)
+  }
+  if (suppress == FALSE)
+    cat("\n\n")
+  return(result)
+}
--- R/otolith-internal.R
+++ R/otolith-internal.R
@@ -1,5 +1,7 @@
 #' @keywords internal 
 
+#---------------------------------------------------------
+
 # function to get extension from a list of full/ half path
 # how it works: split character at ".", take the last splitted part as ext
 # fixed bug for file name with "." in it 
@@ -10,13 +12,17 @@
          character(1), USE.NAMES = FALSE)
 }
 
-# function to get file names from a list of full path
-# how it works: split character at "//" OR "/" (regular expression accepts logical
-#  conditions), take the last part, and if remove.ext=TRUE, substitute the 
-#  ".<ext>" with empty string
+#---------------------------------------------------------
+
+# function to get file names from a list of full path how it works: split
+# character at "//" OR "/" (regular expression accepts logical conditions), take
+# the last part, and if remove.ext=TRUE, substitute the ".<ext>" with empty
+# string
 # @param pathlist list of path of length >=1
 # @param remove.ext specify whether to remove the extension
-# @reference http://stackoverflow.com/questions/19424709/r-gsub-pattern-vector-and-replacement-vector
+# @reference
+#   http://stackoverflow.com/questions/19424709
+# actually same as basename
 .getfilename <- function (pathlist, remove.ext = TRUE) {
   output <- vapply(pathlist, function(x) rev(unlist(strsplit(x, 
             split = "[\\]|/")))[1], character(1), USE.NAMES = FALSE)
@@ -26,20 +32,91 @@
   return(output)
 }
 
+#---------------------------------------------------------
+
 # function to get directory of where the file is from a full path name/ list
 # @param path a single path name or a path list
 # @note if a path list is provided, extraction is based on the first file on 
 #   the list only
 # @return path
+# actually same as dirname
 .getdir <- function(path) {  
   if (length(path) > 1) path <- path[1]
   paste(rev(rev(unlist(strsplit(path, "[\\]|/")))[-1]), collapse="/") 
 }
 
+#---------------------------------------------------------
+
 # function to determine if an object is a otolith project
 # @param x object to be determined
 # @return logical
 .isproj <- function (x) {
-  temp <- c("landmark","des", "nef", "gpa", "pc", "har", "class") %in% unlist(attributes(x))
+  temp <- c("landmark","des", "nef", 
+            "gpa", "pc", "har", "class") %in% unlist(attributes(x))
   !any(temp == FALSE)
+}
+
+#---------------------------------------------------------
+
+# internal function wrapped under agglda()
+
+.kflda <- function(X, Y, newdata = NULL, k = 5, 
+                   prior = c("equal", "proportion")) {
+  require(MASS)
+  Y <- factor(Y)
+  dat <- data.frame(class=Y)
+  dat$predictor <- as.matrix(X)   
+  prior <- match.arg(prior)
+  alltestingindices <- kfcv.testing(dim(X)[1], k = k)  
+  # initiate for aggregating class and posterior 
+  if (is.null(newdata)) {
+    agg.class <- factor(levels = levels(Y))
+    agg.posterior <- matrix(data = NA, dim(X)[1], length(levels(Y)), 
+                            dimnames = list(rownames(X), levels(Y)))
+  } else {
+    agg.class <- as.data.frame(matrix(data = NA, k, dim(newdata)[1], dimnames = 
+                               list(paste0("fold", 1:k), rownames(newdata))))
+    agg.posterior <- array(data = NA, dim = c(dim(newdata)[1], 
+                                              length(levels(Y)), k), 
+                           dimnames = list(rownames(newdata), levels(Y), 
+                                           paste0("fold", 1:k)))
+  }
+  # going thru the folds
+  for (i in 1:k) {
+    testingindices <- alltestingindices[[i]]
+    train <- dat[-testingindices, ]
+    test <- dat[testingindices, ]
+    trainlength <- length(levels(train$class))
+    switch(prior, equal = assign("prior.", rep(1 / trainlength, trainlength)), 
+           proportion = assign("prior.", sapply(summary(train$class), 
+                                                function (x) x / dim(train)[1])))
+    mod.i <- lda(class ~ predictor, data = train, prior = prior.)
+    if (!is.null(newdata)) {
+      temp <- data.frame(newdata)
+      temp$predictor <- as.matrix(newdata)
+      test <- temp 
+    }
+    prediction.temp <- predict(mod.i, test)
+    prediction.i <- prediction.temp$class
+    posterior <- prediction.temp$posterior
+    if (is.null(newdata))
+      agg.class[testingindices] <- prediction.i
+    else 
+      agg.class[i, ] <- prediction.i
+    # if any class did not appear in the training set in this sub-model
+    if (dim(posterior)[2] < dim(agg.posterior)[2]) {
+      posmatindex <- sapply(colnames(posterior), function(x) 
+                     which(x == levels(train$class)))
+      if (is.null(newdata)) # those without the predictions remain NA
+        agg.posterior[testingindices, posmatindex] <- posterior 
+      else
+        agg.posterior[, posmatindex, i] <- posterior
+    } else {
+      if (is.null(newdata))
+        agg.posterior[testingindices, ] <- posterior 
+      else
+        agg.posterior[, , i] <- posterior
+    }     
+  }
+  return(list(agg.class = agg.class, agg.posterior = agg.posterior))
 }
